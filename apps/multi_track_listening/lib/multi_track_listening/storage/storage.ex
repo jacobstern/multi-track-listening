b1fc726de416cb4f8235eb42c2e1a14fe73e9789
Jacob Stern <jacob.stern@outlook.com>
2019-02-27 22:22:44 -0800

    Add Storage.duplicate_file!/1

--- apps/multi_track_listening/lib/multi_track_listening/storage/storage.ex
+++ apps/multi_track_listening/lib/multi_track_listening/storage/storage.ex
@@ -19,11 +19,15 @@ defmodule MultiTrackListening.Storage do
     Path.join([priv, "media", uuid])
   end
 
+  defp create_file_record(content_type) do
+    uuid = UUID.uuid4()
+    Repo.insert!(%Storage.File{backend: "local", content_type: content_type, uuid: uuid})
+  end
+
   @spec persist_file(File.Path.t(), uuid_t()) :: uuid_t()
   def persist_file(file_path, content_type) do
-    uuid = UUID.uuid4()
+    %Storage.File{uuid: uuid} = create_file_record(content_type)
     File.cp!(file_path, generate_local_path(uuid))
-    Repo.insert!(%Storage.File{backend: "local", content_type: content_type, uuid: uuid})
     uuid
   end
 
@@ -52,5 +56,12 @@ defmodule MultiTrackListening.Storage do
   @spec copy_file_locally!(uuid_t(), File.Path.t()) :: :ok
   def copy_file_locally!(uuid, destination_path) do
     File.cp!(generate_local_path(uuid), destination_path)
+  end
+
+  def duplicate_file!(uuid) do
+    file = get_file(uuid)
+    %Storage.File{uuid: new_uuid} = create_file_record(file.content_type)
+    File.cp!(generate_local_path(uuid), generate_local_path(new_uuid))
+    new_uuid
   end
 end
