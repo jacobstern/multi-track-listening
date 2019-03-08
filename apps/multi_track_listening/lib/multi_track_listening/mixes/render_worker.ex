9065d3eeeecc44eacb7cb1da1289e3043377684d
Jacob Stern <jacob.stern@outlook.com>
2019-03-07 19:13:30 -0800

    Surface errors from GCS in worker

--- apps/multi_track_listening/lib/multi_track_listening/mixes/render_worker.ex
+++ apps/multi_track_listening/lib/multi_track_listening/mixes/render_worker.ex
@@ -15,8 +15,12 @@ defmodule MultiTrackListening.Mixes.RenderWorker do
 
   defp update_and_notify(render, updates) do
     updated = Mixes.update_render_internal(render, updates)
-    payload = MixView.render("mix-render.json", mix_render: updated)
-    Endpoint.broadcast!("mix_renders:#{updated.id}", "update", payload)
+
+    Endpoint.broadcast!(
+      "mix_renders:#{updated.id}",
+      "update",
+      MixView.render("mix-render.json", mix_render: updated)
+    )
   end
 
   def do_render(render = %Render{}) do
@@ -29,26 +33,29 @@ defmodule MultiTrackListening.Mixes.RenderWorker do
         &Path.join(temp_directory, &1)
       )
 
-    Storage.download_file!(render.track_one_file_uuid, track_one_path)
-    Storage.download_file!(render.track_two_file_uuid, track_two_path)
+    try do
+      Storage.download_file!(render.track_one_file_uuid, track_one_path)
+      Storage.download_file!(render.track_two_file_uuid, track_two_path)
 
-    with :ok <-
-           Cruncher.crunch_files(track_one_path, track_two_path, destination_path,
-             start_l: render.track_one_start,
-             start_r: render.track_two_start,
-             mix_duration: render.mix_duration
-           ) do
+      :ok =
+        Cruncher.crunch_files(track_one_path, track_two_path, destination_path,
+          start_l: render.track_one_start,
+          start_r: render.track_two_start,
+          mix_duration: render.mix_duration
+        )
+
       result_file_uuid = Storage.upload_file!(destination_path, "audio/mpeg")
       update_and_notify(render, status: :finished, result_file_uuid: result_file_uuid)
-    else
-      _ ->
+    catch
+      error ->
         update_and_notify(render, status: :error)
-    end
+        throw(error)
+    after
+      for path <- [track_one_path, track_two_path, destination_path], File.exists?(path) do
+        File.rm!(path)
+      end
 
-    for path <- [track_one_path, track_two_path, destination_path] do
-      File.rm!(path)
+      File.rmdir!(temp_directory)
     end
-
-    File.rmdir!(temp_directory)
   end
 end
