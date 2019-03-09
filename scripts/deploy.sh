113f965006264332f7e65351b2522c9b7b82b5bc
Jacob Stern <jacob.stern@outlook.com>
2019-03-09 05:13:16 -0800

    Fix deployment, add ingress

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,4 @@
 
 TAG=${1-$(git rev-parse --verify HEAD)}
 
-kubectl set image deployment/multi-track-listening \
-  "multi-track-listening=gcr.io/multi-track-listening/multi_track_listening:$TAG"
+kubectl set image deployment/web "web=gcr.io/multi-track-listening/multi_track_listening:$TAG"
