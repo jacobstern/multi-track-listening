e1085011937f6c667a81c9591c79fd6b80343234
Jacob Stern <jacob.stern@outlook.com>
2019-03-10 19:59:11 -0700

    Echo deployment command in script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,8 @@
 set -o nounset
 set -e
 
+echo_and_run() { echo "$@" ; "$@" ; }
+
 TAG=${1-$("${BASH_SOURCE%/*}/get-default-tag.sh")}
 
-kubectl set image deployment/web "web=gcr.io/multi-track-listening/multi_track_listening:$TAG"
+echo_and_run kubectl set image deployment/web "web=gcr.io/multi-track-listening/multi_track_listening:$TAG"
